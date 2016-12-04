635bc8961551634b124b0150a7c9389992ddc01a
Muliwe <roman.korneev@architech.nyc>
2016-12-04 20:11:23 +0300

    saved cinemas filtering

--- www/js/controllers/list-cinemas.controller.js
+++ www/js/controllers/list-cinemas.controller.js
@@ -4,27 +4,51 @@ angular.module('afisha').controller('ListCinemasController',
     ['$scope', '$state', '$stateParams', 'serverService', 'localStorageService', 'common',
     function($scope, $state, $stateParams, serverService, localStorageService, common) {
         $scope.cinemas = [];
+        $scope.savedCinemas = [];
 
         $scope.city = {};
         $scope.cityId = +$stateParams.cityId;
         $scope.currentCity = common.currentCity;
+        $scope.isCurrentCity = false;
+
+        recountCurrentCity();
 
         $scope.getCityCinemas = function(){
             serverService.fetchCity($scope.cityId, (city, cinemas) => {
                 $scope.city = city;
-                $scope.cinemas = cinemas;
+                $scope.splitCinemas(cinemas);
             });
         };
 
         $scope.saveCity = function () {
             $scope.currentCity = common.currentCity = $scope.city;
             localStorageService.set('currentCity', JSON.stringify($scope.city));
+            recountCurrentCity();
         };
 
-        $scope.openCinema= function (cinema) {
+        $scope.splitCinemas = function (cinemas) {
+            let cinemaIds = common.savedCinemas.map(cinema => cinema.id);
+
+            $scope.cinemas = [];
+            $scope.savedCinemas = [];
+
+            cinemas.forEach(cinema => {
+               if (cinemaIds.includes(cinema.id)) {
+                   $scope.savedCinemas.push(cinema);
+               } else {
+                   $scope.cinemas.push(cinema);
+                }
+            });
+        };
+
+        $scope.openCinema = function (cinema) {
             $state.go('cinema', {
                 cinemaId: cinema.id
             });
         };
+
+        function recountCurrentCity() {
+            $scope.isCurrentCity = $scope.city.id && $scope.city.id === $scope.currentCity.id;
+        }
     }
 ]);
