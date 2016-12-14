f7aaa9407135b21e4c637daed24968545d528248
Roman Korneyev <roman.korneev@architech.nyc>
2016-12-14 14:38:06 +0300

    404 error + codestyle

--- server/server.js
+++ server/server.js
@@ -14,53 +14,65 @@ app.use(compression());
 app.use(bodyParser.json({extended: false}));
 app.use(cors());
 
-app.use('/cities', function(req, res, next) {
-  res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
-  res.end(JSON.stringify(data.cities));
+app.use('/cities', function (req, res, next) {
+    res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
+    res.end(JSON.stringify(data.cities));
 });
 
-app.use('/cinemas', function(req, res, next) {
-  const cityId = parseInt(req.originalUrl.replace('/cinemas/', ''), 10);
+app.use('/cinemas', function (req, res, next) {
+    const cityId = parseInt(req.originalUrl.replace('/cinemas/', ''), 10);
 
-  res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
+    res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
 
-  if (!cityId && isNaN(cityId)) {
-      res.end(JSON.stringify(data.cinemas));
-  } else {
-      res.end(JSON.stringify(data.cinemas.filter(function (cinema) {
-          return cinema.city === cityId;
-      })));
-  }
+    if (!cityId && isNaN(cityId)) {
+        res.end(JSON.stringify(data.cinemas));
+    } else {
+        res.end(JSON.stringify(data.cinemas.filter(function (cinema) {
+            return cinema.city === cityId;
+        })));
+    }
 });
 
-app.use('/film/', function(req, res, next) {
-  const filmId = parseInt(req.originalUrl.replace('/film/', ''), 10);
+app.use('/film/', function (req, res, next) {
+    const filmId = parseInt(req.originalUrl.replace('/film/', ''), 10);
 
-  res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
-  res.end(JSON.stringify(data.films.filter(function (film) {
-      return film.id === filmId;
-  })[0]));
+    res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
+    res.end(JSON.stringify(data.films.filter(function (film) {
+        return film.id === filmId;
+    })[0]));
 });
 
-app.use('/show/', function(req, res, next) {
-  const filmId = parseInt(req.originalUrl.replace(/^\/show\/([0-9]+)\/.*$/, '$1'), 10);
-  const cinemaId = parseInt(req.originalUrl.replace(/^\/show\/([0-9]+)\/(.*)$/, '$2'), 10);
+app.use('/show/', function (req, res, next) {
+    const filmId = parseInt(req.originalUrl.replace(/^\/show\/([0-9]+)\/.*$/, '$1'), 10);
+    const cinemaId = parseInt(req.originalUrl.replace(/^\/show\/([0-9]+)\/(.*)$/, '$2'), 10);
 
-  res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
-  res.end(JSON.stringify(data.shows.filter(function (show) {
-      return show.film === filmId && show.cinema === cinemaId;
-  })));
+    res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
+    res.end(JSON.stringify(data.shows.filter(function (show) {
+        return show.film === filmId && show.cinema === cinemaId;
+    })));
 });
 
 app.use(serveStatic(__dirname + '/../www/'));
 
-app.use('/', function(req, res, next) {
-  res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
-  res.end(JSON.stringify({
-      started: started.toISOString(),
-      running: timeDiffFormat((new Date().getTime() - started.getTime()) / 1000),
-      originalUrl: req.originalUrl
-  }));
+app.use('/', function (req, res, next) {
+    if (req.originalUrl === '/') {
+        res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
+        res.end(JSON.stringify({
+            started: started.toISOString(),
+            running: timeDiffFormat((new Date().getTime() - started.getTime()) / 1000),
+            originalUrl: req.originalUrl
+        }));
+    } else {
+        res.writeHead(404, {"Content-Type": "application/json; charset=utf-8"});
+        res.end(JSON.stringify({
+            error: {
+                name: 'Error',
+                status: 404,
+                message: `There is no method to handle GET ${req.originalUrl}`,
+                statusCode: 404
+            }
+        }));
+    }
 });
 
 http.createServer(app).listen(process.env.PORT || 3000);
