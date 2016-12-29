9d3bfbf684f32b87edf79c85632dfcdd30b94b67
Roman Korneyev <roman.korneev@architech.nyc>
2016-12-29 11:52:42 +0300

    films list fetching

--- www/js/controllers/list-films.controller.js
+++ www/js/controllers/list-films.controller.js
@@ -1,33 +1,36 @@
 'use strict';
 
 angular.module('afisha').controller('ListFilmsController',
-    ['$scope', '$state',
-    function($scope, $state) {
-        $scope.films = [
-            {"id":1,"title":"Доктор Стрэндж","age":"16","shows":58274,"poster":"http://s1.kassa.rl0.ru/StaticContent/P/Img/1609/28/160928110516678.jpg", "anons": "Главная героиня общается с инопланетянами и вспоминает погибшую дочь"},
-            {"id":2,"title":"Инферно","age":"16","shows":5843,"poster":"http://s2.kassa.rl0.ru/StaticContent/P/Img/1610/11/161011103842547.jpg", "anons": "Профессор Лэнгдон снова спасает мир"},
-            {"id":3,"title":"Расплата","age":"18","shows":17005,"poster":"http://s2.kassa.rl0.ru/StaticContent/P/Img/1610/20/161020175830989.jpg", "anons": "Аутический триллер про криминального бухгалтера"},
-            {"id":4,"title":"Джек Ричер 2: Никогда не возвращайся","age":"16","shows":4139,"poster":"http://s1.kassa.rl0.ru/StaticContent/P/Img/1610/04/161004151752356.jpg", "anons": "Проходной боевик с Крузом"},
-            {"id":5,"title":"Ледокол","age":"12","shows":19312,"poster":"http://s1.kassa.rl0.ru/StaticContent/P/Img/1610/04/161004152034277.jpg", "anons": "Советские моряки против айсберга"},
-            {"id":6,"title":"Дуэлянт","age":"16","shows":1600,"poster":"http://s1.kassa.rl0.ru/StaticContent/P/Img/1609/08/160908104500630.jpg", "anons": "Сложносочинённая роосийская костюмная дама о проклятии в позапрошлом веке"}
-        ];
+    ['$scope', '$state', 'common', 'serverService',
+    function($scope, $state, common, serverService) {
+        $scope.films = [];
 
-        $scope.getFilmList = function(){
-            let resourse = null;
+        $scope.currentCity = null;
 
-            return resourse;
+        $scope.getCitiesList = function(city) {
+            $scope.films = [];
+            serverService.fetchFilms(city, (err, films) => {
+                $scope.films = films;
+            });
         };
 
-        $scope.refreshList = function () {
-            $scope.getFilmList().$promise.finally(function () {
+        $scope.refreshList = function() {
+            $scope.getCitiesList(function() {
                 $scope.$broadcast('scroll.refreshComplete');
             });
         };
 
-        $scope.openFilm = function (film) {
+        $scope.openFilm = function(film) {
             $state.go('film', {
                 filmId: film.id
             });
         };
+
+        $scope.toggleCity = function() {
+            $scope.currentCity = $scope.currentCity ? null : common.currentCity;
+            $scope.getCitiesList($scope.currentCity);
+        };
+
+        $scope.toggleCity();
     }
 ]);
--- www/js/services/server.service.js
+++ www/js/services/server.service.js
@@ -41,12 +41,34 @@ angular.module('afisha').service('serverService', function($http, common) {
             responseType: 'json'
         }).then(function successCallback(response) {
             if (!response.data.originalUrl) {
-                common.cache.cinemas = response.data.sort(function(a, b){
+                common.cache.cinemas = response.data.sort((a, b) => {
                     if(a.title < b.title) return -1;
                     if(a.title > b.title) return 1;
                     return 0;
                 });
                 cb(null, common.cache.cinemas);
+            } else {
+                cb('Wrong url', []);
+            }
+        }, function errorCallback(response) {
+            cb(response.data || 'Request failed', []);
+        });
+    };
+
+    self.fetchFilms = (city, cb) => {
+        $http({
+            method: 'GET',
+            url: `${common.serverUrl}/films${city ? '/' + city.id : ''}`,
+            cache: true,
+            responseType: 'json'
+        }).then(function successCallback(response) {
+            if (!response.data.originalUrl) {
+                var films = response.data.sort((a, b) => {
+                    if(a.shows < b.shows) return 1;
+                    if(a.shows > b.shows) return -1;
+                    return 0;
+                });
+                cb(null, films);
             } else {
                 cb('Wrong url', []);
             }
