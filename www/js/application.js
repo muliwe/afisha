0fed95174bce3fd4700e80015f8d2fee33d8f2c1
Roman Korneyev <roman.korneev@architech.nyc>
2017-01-07 17:46:55 +0300

    less logs

--- www/js/application.js
+++ www/js/application.js
@@ -212,7 +212,7 @@ const ApplicationConfiguration = (function(){
             }
 
             serverService.fetchCities((err, cities) => {
-                console.log(err, cities);
+                // console.log(err, cities);
             });
         });
 
